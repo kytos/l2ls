6cce026ea50afff6a01746dfc362fc60f814f81d
exterminus <cmagnobarbosa@gmail.com>
2019-02-25 09:01:28 -0300

    Fixing linter issues

--- settings.py
+++ settings.py
@@ -1,7 +1,9 @@
 """Settings for the of_l2ls NApp."""
 # LLDP Mac Addresses
-lldp_macs = ['01:80:c2:00:00:0e', '01:80:c2:00:00:03',
+LLDP_MACS = ['01:80:c2:00:00:0e', '01:80:c2:00:00:03',
              '01:80:c2:00:00:00']
 
 # Flow priority for flows installed by this NApp
-flow_priority = 10
+
+# flow_priority = 10
+FLOW_PRIORITY = 10
